5376378ba5ac38caebf4f568eb9c20592760cf54
MenendezON <menendezon@gmail.com>
2023-08-26 10:54:15 +0000

    Update src folder

---
+++ src/cartItems.js
@@ -0,0 +1,23 @@
+const cartItems = [
+    {
+        id: 'qefexvvvvv',
+        title: 'XP 1',
+        price: '300.0',
+        img: '',
+        amount:1,
+    },
+    {
+        id: 'qefevvavvv',
+        title: 'XP 1',
+        price: '300.0',
+        img: '',
+        amount:1,
+    },
+    {
+        id: 'qefevvvvvv',
+        title: 'XP 1',
+        price: '300.0',
+        img: '',
+        amount:1,
+    },
+]
